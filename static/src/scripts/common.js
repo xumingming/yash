363fe60862fd2be545440c4383a11fd5c34bc41d
James Xu <xumingmingv@gmail.com>
2015-08-16 21:26:38 +0800

    implemented shortcut j to go to upper folder

--- static/src/scripts/common.js
+++ static/src/scripts/common.js
@@ -8,4 +8,18 @@ $(function(){
 
     // initialize all popover
     $('.J-popover').popover();
+
+    document.onkeydown = checkKey;
+    function checkKey(e) {
+        e = e || window.event;
+
+        if (e.keyCode == '74') { // 'j' to go to parent folder
+            var newhref = window.location.href
+            var idx = newhref.lastIndexOf("/")
+            if (idx >= 0) {
+                newhref = newhref.substr(0, idx)
+                window.location = newhref;
+            }
+        }
+    }
 });
