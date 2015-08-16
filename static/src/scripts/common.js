32200f8fc8b655d10453f36e134da9b43b9c1dc3
James Xu <xumingmingv@gmail.com>
2015-08-16 22:30:20 +0800

    fix shortcut bug

--- static/src/scripts/common.js
+++ static/src/scripts/common.js
@@ -9,13 +9,25 @@ $(function(){
     // initialize all popover
     $('.J-popover').popover();
 
+    function isInSearchBox() {
+        var ae = document.activeElement;
+        if (ae) {
+            ae = $(ae);
+            if (ae.attr("name") == "w") {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
     document.onkeydown = checkKey;
     function checkKey(e) {
         e = e || window.event;
 
         console.log("keycode: " + e.keyCode);
 
-        if (e.keyCode == '72') { // 'h' to go to home page
+        if (e.keyCode == '72' && !isInSearchBox()) { // 'h' to go to home page
             var newhref = window.location.href
             var idx = newhref.indexOf("/")
             idx = newhref.indexOf("/", idx + 1)
@@ -27,7 +39,7 @@ $(function(){
             }
         }
         
-        if (e.keyCode == '85') { // 'u' to go to parent folder
+        if (e.keyCode == '85' && !isInSearchBox()) { // 'u' to go to parent folder
             var newhref = window.location.href
             var idx = newhref.lastIndexOf("/")
             if (idx >= 0) {
@@ -36,14 +48,14 @@ $(function(){
             }
         }
 
-        if (e.keyCode == '83') { // 's' to search
+        if (e.keyCode == '83' && !isInSearchBox()) { // 's' to search
             var searchBox = $("input[name=w]")[0];
             searchBox = $(searchBox);
             searchBox.focus();
             e.preventDefault();
         }
 
-        if (e.keyCode == '65') { // 'a' to focus the first item in listing page
+        if (e.keyCode == '65' && !isInSearchBox()) { // 'a' to focus the first item in listing page
             var items = $(".list-group")
             if (items) {
                 var firstItem = items.children()[0];
@@ -62,7 +74,7 @@ $(function(){
             }
         }
         
-        if (e.keyCode == '75') { // 'k' to go up one item in listing page
+        if (e.keyCode == '75' && !isInSearchBox()) { // 'k' to go up one item in listing page
             var ae = document.activeElement;
             if (ae && ae.tagName == "A" && $(ae).parent()[0].tagName == "LI") {
                 ae = $(ae);
